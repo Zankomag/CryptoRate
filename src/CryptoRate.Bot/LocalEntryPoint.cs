a6deb2d31d7e943e4ad635a1d75f0c7b66a4d8e3
Adam Evans <Zankomag@gmail.com>
2021-09-26 19:25:41 +0300

    Add local entry point

---
+++ src/CryptoRate.Bot/LocalEntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using CryptoRate.Bot.Services;
+using CryptoRate.Core.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CryptoRate.Bot {
+
+	public class LocalEntryPoint {
+
+		private static void Main(string[] args) {
+			var host = new HostBuilder()
+				.AddConfiguration()
+				.UseStartup<Core.Startup>()
+				.UseStartup<Startup>()
+				.ConfigureServices(services => services.AddHostedService<TelegramBotLocalHostedService>())
+				.Build();
+
+			host.Run();
+		}
+
+	}
+
+}

a8bf4821da2c3543572b3de05191d320467f7f1e
Adam Evans <Zankomag@gmail.com>
2021-10-13 17:32:13 +0300

    Fix configuration initialization

--- src/CryptoRate.Bot/LocalEntryPoint.cs
+++ src/CryptoRate.Bot/LocalEntryPoint.cs
@@ -10,8 +10,8 @@ namespace CryptoRate.Bot {
 	public class LocalEntryPoint {
 
 		private static async Task Main() {
-			var host = GetHost();
-			//var host = GetWebHost();
+			//var host = GetHost();
+			var host = GetWebHost();
 
 			await host.RunAsync();
 		}
@@ -26,11 +26,11 @@ namespace CryptoRate.Bot {
 
 		private static IHost GetWebHost()
 			=> new HostBuilder()
+				.AddConfiguration()
 				.ConfigureWebHost(x =>
 					x.UseKestrel()
 						.UseUrls("https://*:5930")
 						.UseStartup<WebStartup>())
-				.AddConfiguration()
 				.Build();
 
 	}

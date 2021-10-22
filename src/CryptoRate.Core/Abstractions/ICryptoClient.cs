0d737fb78324ee36d2603683ad3fb128a01141c7
Adam Evans <Zankomag@gmail.com>
2021-10-22 12:50:54 +0300

    Move bot message template to config, change Currency to CurrencyCode, add handling for currency without char

--- src/CryptoRate.Core/Abstractions/ICryptoClient.cs
+++ src/CryptoRate.Core/Abstractions/ICryptoClient.cs
@@ -5,6 +5,7 @@ namespace CryptoRate.Core.Abstractions {
 
 	public interface ICryptoClient {
 
+		//TODO add new format that contains char fields too and map to it via CurrencyCode extensions and mapper
 		Task<Exchangerate> GetCurrencyRate(string currencyBase, string currencyQuote);
 
 	}
---
+++ src/CryptoRate.Core/Enums/CurrencyCode.cs
@@ -0,0 +1,30 @@
+﻿namespace CryptoRate.Core.Enums {
+
+	/// <summary>
+	///     Represents constants with currency codes and chars
+	/// </summary>
+	public static class CurrencyCode {
+
+		public const string Bitcoin = "BTC";
+		public const string BitcoinChar = "₿";
+		
+		public const string Ethereum = "ETH";
+		public const string EthereumChar = "Ξ";
+
+		/// <summary>
+		///     United States Dollar
+		/// </summary>
+		public const string Usd = "USD";
+		public const string UsdChar = "$";
+
+		public static string GetCurrencyCharByCode(this string currencyCode)
+			=> currencyCode switch {
+				Bitcoin => BitcoinChar,
+				Ethereum => EthereumChar,
+				Usd => UsdChar,
+				_ => currencyCode
+			};
+
+	}
+
+}

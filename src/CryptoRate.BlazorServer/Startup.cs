e396dfdd220207c462f07f8099266cefc296ae6f
Adam Evans <Zankomag@gmail.com>
2021-10-25 02:12:03 +0300

    Add Syncfusion.Blazor and Core referenses to BlazorServer project

--- src/CryptoRate.BlazorServer/Startup.cs
+++ src/CryptoRate.BlazorServer/Startup.cs
@@ -6,12 +6,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CryptoRate.Common.Utils;
+using CryptoRate.Core.Extensions;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 
 namespace CryptoRate.BlazorServer
 {
@@ -27,16 +25,20 @@ namespace CryptoRate.BlazorServer
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+			services.AddCryptoRateServices(Configuration);
+			
 			services.AddLogging(logging => logging.AddConsole());
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+			services.AddSyncfusionBlazor();
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() || EnvironmentWrapper.IsDevelopment)
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Configuration.GetSection("Syncfusion:LicenseKey").Value);
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
